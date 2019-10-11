4c92910b143af5361dcd405457b6748c23a3f3f5
abimaelrsergio <abimaelr.sergio@gmail.com>
2019-10-10 22:57:37 -0300

    Quando tem pouco texto no html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<h1>Salve amigos, como estão?</h1>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
