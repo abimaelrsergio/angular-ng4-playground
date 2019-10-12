d580477a88d14ef3fc19fe62464c6abe4cd33a98
abimaelrsergio <abimaelr.sergio@gmail.com>
2019-10-11 23:23:58 -0300

    Animate parte 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,9 +25,11 @@ import { trigger,state,style,transition,animate,keyframes } from '@angular/anima
       state('large', style({
         transform: 'scale(1.2)',
       })),
-      transition('small <=> large', animate('300ms ease-in', style({
-        transform: 'translateY(40px)'
-      }))),  
+      transition('small <=> large', animate('300ms ease-in', keyframes([
+        style({opacity: 0, transform: 'translateY(-75%)', offset: 0}), 
+        style({opacity: 1, transform: 'translateY(35px)', offset: .5}), 
+        style({opacity: 1, transform: 'translateY(0)', offset: 1}), 
+      ]))),  
     ]),
   ]
 
